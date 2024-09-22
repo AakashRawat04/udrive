7925b96a88f04e8604ad18439945dc56f4d373af
Ash <01tripster@gmail.com>
2024-09-22 16:55:06 +0530

    feat: add db client

---
+++ backend/utils/db.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import process from "node:process";
+import postgres from "postgres";
+
+export const db = drizzle(
+	postgres({
+		host: process.env.HOST!,
+		user: process.env.USER_NAME!,
+		password: process.env.PASSWORD!,
+		port: Number(process.env.PORT!),
+		database: process.env.DATABASE!,
+	})
+);
